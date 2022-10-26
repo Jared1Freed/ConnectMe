908d680e641b44c33286e9109c052fb61108bc37
Dtaylor1333 <58613334+Dtaylor1333@users.noreply.github.com>
2022-10-26 16:55:33 -0500

    Add files via upload

--- VS Studio Code/new.js
+++ VS Studio Code/new.js
@@ -8,6 +8,28 @@ function myFunction() {
     }
   }
 
+/*
+  This is for the Show password Toggle Icon
+
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+
+togglePassword.addEventListener("click", function (e) {
+      // toggle the type attribute
+      const type = password.getAttribute("type") === "password" ? "text" : "password";
+      password.setAttribute("type", type);
+      
+      // toggle the icon
+      this.classList.toggle("bi-eye");
+  });
+
+  // prevent form submit
+const form = document.querySelector("form");
+form.addEventListener('submit', function (e) {
+      e.preventDefault();
+  });
+
+*/
 
 /*
 mybutton = document.getElementById("scrollUp");
@@ -31,3 +53,4 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 */
+
